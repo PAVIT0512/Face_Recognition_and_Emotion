3b5c468d4b786a3b2d499671a8a80e3cf4d96210
PAVIT SINGH NARANG <pavitnarang14@gmail.com>
2024-07-22 04:51:40 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-# Face_Recognition_and_Emotion
+Hello Viewers<br><br>
+This is a trained CNN classifier using Keras with a TensorFlow backend, capable of successfully predicting various human emotions 😊😢😡😱. The classifier can distinguish between multiple emotional states such as happiness, sadness, anger, and fear. After training the model, I integrated it with OpenCV and the face_recognition library to detect faces in real-time video streams.<br> Once a face is detected, it is passed to the classifier, which then predicts the person's emotion. This combination of deep learning and real-time face detection allows for an interactive and dynamic application, capable of understanding human emotions on the fly. <br>The system can draw bounding boxes around detected faces and display the predicted emotions using both text labels. This project demonstrates the powerful synergy between machine learning, computer vision, and real-time image processing technologies 🖥️📸🤖.<br><br>
+You can view the demonstration in the provided video link 🎥:<br>
+https://drive.google.com/file/d/1MoRPzgczFvYnTK6EWHoqH-pw_ZCpfrRf/view?usp=sharing<br><br>
+First make sure you have entered the virtual python environment.To enter venv use the command:<br>
+**.\myenv\Scripts\activate**<br><br>
+If you want to run the program in local environment then kindly install these packages using the following command:<br>
+**pip install opencv-python face_recognition imutils matplotlib pandas tqdm tensorflow**<br><br>
+
+Note: I recommend to use the virtual environment as it will help you run the code without any errors.<br><br>
+
+**Face Recognition**<br>
+To build your dataset for Face Recognition, first create your own folder with your name inside the dataset folder. You can add your own images to the folder, or use the provided Python script called **build_face_dataset.py** to take photos from the local system. 📷<br>
+To use this script, open build_face_dataset.py and replace line 49,**p = os.path.sep.join(["dataset/pavit", "{}.png".format(str(total).zfill(5))])**, with **p = os.path.sep.join(["dataset/{Your Name}", "{}.png".format(str(total).zfill(5))])**, then run the code. While running the script, press **"k"** to capture images and **"q"** to quit. This will save the photos in the folder you created. 📁<br><br>
+After capturing the images, encode them by running **encode_faces.py**. This will create an encodings.pickle file, storing the encodings of the images. Once this is done, run **pi_face_recognition.py** and enjoy the experience. 🎉<br><br>
+**Emotion**<br>
+To run this from scratch, first prepare the dataset by running the **dataset_prepare.py** file. To generate the **model.h5** file, run **emotion_model_builder.py**. After that, run the **emotions.py** script and enjoy the experience. 😊.<br><br>
+**Emotion+Face Recognition**<br>
+For a combined experience, run the **emotion+face_recognition.py** script and enjoy the seamless integration of emotion detection and face recognition. 🤖
+
+
+
+
+
+
+
+
---
+++ emotion+face_recogniton.py
@@ -0,0 +1,90 @@
+import numpy as np
+import cv2
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout, Flatten, Conv2D, MaxPooling2D
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from imutils.video import VideoStream, FPS
+import face_recognition
+import imutils
+import pickle
+import time
+
+
+model = Sequential()
+
+model.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=(48, 48, 1)))
+model.add(Conv2D(64, kernel_size=(3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+
+model.add(Conv2D(128, kernel_size=(3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Conv2D(128, kernel_size=(3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+
+model.add(Flatten())
+model.add(Dense(1024, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(7, activation='softmax'))
+
+
+model.load_weights('model.h5')
+data = pickle.loads(open("encodings.pickle", "rb").read())
+cv2.ocl.setUseOpenCL(False)
+emotion_dict = {0: "Angry", 1: "Disgusted", 2: "Fearful", 3: "Happy", 4: "Neutral", 5: "Sad", 6: "Surprised"}
+
+print("[INFO] Starting video stream...")
+vs = VideoStream(src=0).start()
+time.sleep(2.0)
+fps = FPS().start()
+
+while True:
+    frame = vs.read()
+    frame = imutils.resize(frame, width=500)
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    facecasc = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+    faces = facecasc.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=5, minSize=(30, 30), flags=cv2.CASCADE_SCALE_IMAGE)
+    boxes = [(y, x + w, y + h, x) for (x, y, w, h) in faces]
+    encodings = face_recognition.face_encodings(rgb, boxes)
+    names = []
+    for encoding in encodings:
+        matches = face_recognition.compare_faces(data["encodings"], encoding)
+        name = "Unknown"
+
+        if True in matches:
+            matchedIdxs = [i for (i, b) in enumerate(matches) if b]
+            counts = {}
+            for i in matchedIdxs:
+                name = data["names"][i]
+                counts[name] = counts.get(name, 0) + 1
+            name = max(counts, key=counts.get)
+        names.append(name)
+
+    
+    for ((top, right, bottom, left), name) in zip(boxes, names):
+        cv2.rectangle(frame, (left, top), (right, bottom), (0, 255, 0), 2)
+        y = top - 15 if top - 15 > 15 else top + 15
+        cv2.putText(frame, name, (left, y), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0, 255, 0), 2)
+        roi_gray = gray[top:bottom, left:right]
+        cropped_img = np.expand_dims(np.expand_dims(cv2.resize(roi_gray, (48, 48)), -1), 0)
+        prediction = model.predict(cropped_img)
+        maxindex = int(np.argmax(prediction))
+        cv2.putText(frame, emotion_dict[maxindex], (left, bottom + 20), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
+
+    
+    cv2.imshow('Video', frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+    fps.update()
+
+fps.stop()
+
+print("[INFO] Elapsed time: {:.2f}".format(fps.elapsed()))
+print("[INFO] Approx. FPS: {:.2f}".format(fps.fps()))
+
+# Cleanup
+cv2.destroyAllWindows()
+vs.stop()
